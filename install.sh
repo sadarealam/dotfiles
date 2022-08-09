2ebb487dc11b61dd8a66d2cde5f6a045cf72c3e4
Sadare Alam <sadarealam@Sadares-MacBook-Pro.local>
2022-08-09 11:27:27 +0530

    install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+history | grep 'brew install'
+brew install openjdk@17
+brew install starship
+brew install peco
+brew install exa
+brew install tmux
+brew install visual-studio-code
+brew install fnm
