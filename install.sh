e80b577f51653c080c3291bbc371737c6a9886f9
Sadare Alam <itisalam@gmail.com>
2022-08-09 12:52:17 +0530

    update optional software

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
+brew install fish
+brew install alacritty
+brew install firefox
+brew install tmux
+brew install mysqlworkbench
+brew install mysql
 brew install ninja libtool automake cmake pkg-config gettext curl
 brew install openjdk@17
 brew install starship
 brew install peco
 brew install exa
-brew install tmux
 brew install visual-studio-code
 brew install fnm
