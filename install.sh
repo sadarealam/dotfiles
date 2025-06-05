67b8fe34258f954b463968fd468a0259acd7bd97
sadar.ealam <sadare.alam@sunking.com>
2025-06-06 01:55:15 +0530

    add install script for starter apps

---
+++ install.sh
@@ -0,0 +1,8 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install alacritty
+brew install zelli
+brew install starship
+brew install --cask vlc
+brew install --cask visual-studio-code
+brew install stow
+brew install neovim
